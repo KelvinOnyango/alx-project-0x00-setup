023223363644d2a8c904f924d6be056325bbd11f
Kelvin Onyango <kelvinonyango155@gmail.com>
2025-05-31 19:52:01 -0700

    Your commit message here

--- alx-project-0x00/pages/about.tsx
+++ alx-project-0x00/pages/about.tsx
@@ -1,8 +1,15 @@
 const About: React.FC = () => {
   return (
-    <div>
+    <div className="p-8">
       <h1 className="text-xl font-extralight">About Page</h1>
+      <p className="mt-4">
+        This is an Airbnb clone project built with Next.js.
+      </p>
+      <a href="/" className="mt-4 inline-block text-blue-500 hover:underline">
+        Back to Home
+      </a>
     </div>
   );
 };
+
 export default About;
--- alx-project-0x00/pages/landing.tsx
+++ alx-project-0x00/pages/landing.tsx
@@ -1,8 +1,13 @@
 const Landing: React.FC = () => {
   return (
-    <div>
+    <div className="p-8">
       <h1 className="text-xl font-extralight">Landing Page</h1>
+      <p className="mt-4">Welcome to the Airbnb clone landing page.</p>
+      <a href="/" className="mt-4 inline-block text-blue-500 hover:underline">
+        Back to Home
+      </a>
     </div>
   );
 };
+
 export default Landing;
